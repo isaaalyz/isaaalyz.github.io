0f0a93b3f9b56c41b7498eb69e5d9cd76e3281aa
isadoralyz <isadoralyz3@gmail.com>
2025-06-10 16:39:13 -0300

    provaaaaaaaaaaaaaaaaaaaaaaaaa

--- task-manager/src/App.svelte
+++ task-manager/src/App.svelte
@@ -5,16 +5,46 @@
     import { fade } from 'svelte/transition'
 
     let invisible= true
+    let tarefa, descricao, prazo, prioridade
 
-	let data = [...
+	let data = [
 		{id: 1, tarefa: "Fazer compras", descricao: "Comprar frutas, legumes e pão", prioridade: "Média", prazo: "12/06/2025", status: false },
 		{id: 2, tarefa: "Estudar JavaScript", descricao: "Revisar conceitos de Promises e Async/Await", prioridade: "Alta", prazo: "10/06/2025", status: true },
 		{id: 3, tarefa: "Pagar contas", descricao: "Pagar conta de luz e internet", prioridade: "Alta", prazo: "14/06/2025", status: false }
 	]
 
+    
+
+    function add() {
+        
+        let aux = data
+        
+        let topico = {
+
+            id: data.length +1,
+            tarefa: tarefa,
+            descricao: descricao,
+            prioridade: prioridade,
+            prazo: prazo,
+            status: false
+
+        }
+
+        aux.push(topico)        
+        data = aux
+    }
+
     function close() {
         invisible= false
     }
+
+    function remove(index){
+        data.splice(index, 1)
+        data=[...data];
+
+    }
+
+    
 
 </script>
 
@@ -25,15 +55,15 @@
         <div class="grid-auto">
             <div>
                 <label for="tarefa">Tarefa</label>
-                <input type="text" name="tarefa"/>
+                <input type="text" name="tarefa" bind:value={tarefa}/>
             </div>
             <div>
                 <label for="prazo">Prazo</label>
-                <input type="date" name="prazo"/>
+                <input type="date" name="prazo"bind:value={prazo}/>
             </div>
             <div>
                 <label for="prioridade">Prioridade</label>
-                <select name="prioridade">
+                <select name="prioridade"bind:value={prioridade}>
                     <option value="Baixa">Baixa</option>
                     <option value="Média">Média</option>
                     <option value="Alta">Alta</option>
@@ -44,10 +74,10 @@
         <div class="grid-conf">
             <div>
                 <label for="descricao">Descrição</label>
-                <textarea name="descricao"></textarea>
+                <textarea name="descricao">bind:value={descricao}</textarea>
             </div>
             <div class="div-btn">
-                <button>
+                <button on:click={() => add()}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFF" class="bi bi-floppy-fill" viewBox="0 0 16 16">
                         <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0H3v5.5A1.5 1.5 0 0 0 4.5 7h7A1.5 1.5 0 0 0 13 5.5V0h.086a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5H14v-5.5A1.5 1.5 0 0 0 12.5 9h-9A1.5 1.5 0 0 0 2 10.5V16h-.5A1.5 1.5 0 0 1 0 14.5z"/>
                         <path d="M3 16h10v-5.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5zm9-16H4v5.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5zM9 1h2v4H9z"/>
@@ -58,7 +88,7 @@
     </div>
 	
     
-	<Tabela/>
+	<Tabela tasks={data}/>
 
 
     {#if invisible}
